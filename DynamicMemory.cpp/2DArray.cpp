347674b1c65269f4f416f1b7a0a5a3a036f61868
Balkrishna6420 <your-bkrishnajha494@gmail.com>
2025-03-31 18:40:43 +0530

    DynamicAllocation

--- DynamicMemory.cpp/2DArray.cpp
+++ DynamicMemory.cpp/2DArray.cpp
@@ -1,24 +1,31 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n;
-    cin>>n;
-    int** arr=new int*[n];
-    for(int i=0;i<n;i++){
-        arr[i]=new int[n];
+    int row;
+    cin>>row;
+    int col;
+    cin>>col;
+    int** arr=new int*[row];
+    for(int i=0;i<row;i++){
+        arr[i]=new int[col];
     }//creation done
     //taking input
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
             cin>>arr[i][j];
         }
     }
     //taking output
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
             cout<<arr[i][j];
         }
         cout<<endl;
     }
+    //deletion of array from heap memory;
+    for(int i=0;i<row;i++){
+        delete []arr[i];
+    }
+    delete []arr;
 
 }
